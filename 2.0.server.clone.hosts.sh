3779b5062b35f7db862399073cebe97640994bbd
Namsik Jung <nsjung74@gmail.com>
2020-07-08 18:03:56 +0900

    hosts 수정

--- 2.0.server.clone.hosts.sh
+++ 2.0.server.clone.hosts.sh
@@ -5,10 +5,12 @@
 #hostnamectl set-hostname pg03
 
 
-HOST1="192.168.79.12 pg01.localnet pg01"
-HOST2="192.168.79.13 pg02.localnet pg02"
+HOST1="192.168.79.16 pg01.localnet pg01"
+HOST2="192.168.79.17 pg02.localnet pg02"
+HOST2="192.168.79.18 pg02.localnet pg02"
 
 [ $(grep -Fxc "${HOST1}" /etc/hosts) -eq 0 ] && echo "${HOST1}" >>  /etc/hosts
+[ $(grep -Fxc "${HOST2}" /etc/hosts) -eq 0 ] && echo "${HOST2}" >>  /etc/hosts
 [ $(grep -Fxc "${HOST2}" /etc/hosts) -eq 0 ] && echo "${HOST2}" >>  /etc/hosts
 
 #ssh pg02
